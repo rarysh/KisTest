40642addf59ec202c9e6d405bc6205f1324d73a1
Rarysh <rarysh@digitalrepublic.com.br>
2021-05-26 20:53:56 -0300

    .

--- src/Funcionality/index.js
+++ src/Funcionality/index.js
@@ -5,74 +5,102 @@ import { useState } from "react";
 
 const Funcionality = () => {
   const [stringInsert, setStringInsert] = useState("");
-  const [stringArr, setStringArr] = useState([]);
-  const [flagUppercase, setFlagUppercase] = useState(false);
 
-  const program = () => {
-    const string = stringInsert.split(" ");
+  const regexOnlyCharacter = /[^a-zA-Z]+/;
+
+  const kisTest = () => {
     let finalString = "";
+    const string = stringInsert.split(" ");
     string.forEach((s) => {
-      const str = singleWord(s);
-      finalString += str + " ";
+      finalString += requirement1(s.normalize("NFD").replace(/[\u0300-\u036f]/g, "")) + " ";
     });
 
-    finalString.substring(0, finalString.length - 1);
-
-    if (flagUppercase) {
-      finalString =
-        finalString.charAt(0).toUpperCase() +
-        finalString.substring(1, finalString.length);
-    }
-
-    console.log(finalString);
+    if (finalString.length > 0)
+      finalString.substring(0, finalString.length - 1);
+    console.log("String final: " + finalString);
   };
 
-  const singleWord = (stringInsert) => {
-    const string = stringInsert.split("");
+  const requirement1 = (str) => {
+    let stringArr = str.split("");
+    let flagSymbol = false;
 
-    if (string) {
-      const regExp = /^[^bcdfghjklmnpqrstzvwx]+$/i;
+    stringArr.forEach((s) => {
+      if (s.search(regexOnlyCharacter)) {
+        flagSymbol = true;
+      }
+    });
+    if (!flagSymbol) {
+      console.log("requirement1: " + stringArr.join(""));
+      return stringArr.join("");
+    }
+    console.log("requirement1: " + stringArr.join(""));
+    return requirement2(stringArr);
+  };
+  const requirement2 = (string) => {
+    let symbol = "";
+    string.forEach((s, i, obj) => {
+      if (!s.search(regexOnlyCharacter)) {
+        symbol += s;
+      }
+    });
+    const result = string.filter((x) => x.search(regexOnlyCharacter));
+    console.log(
+      "requirement2: " +
+        result.join("") +
+        symbol
+    );
+    return requirement3(result) + symbol;
+  };
 
-      const vowel = ["a", "e", "i", "o", "u", "y"];
+  const requirement3 = (string) => {
+    let flagUppercase = false;
+    if (string[0] !== string[0].toLowerCase()) {
+      flagUppercase = true;
+    }
+    let response = requirement6(string);
+    if (flagUppercase)
+      response = response.charAt(0).toUpperCase() + response.slice(1);
+    console.log("requirement3: " + response);
+    return response;
+  };
+  const requirement4 = (str) => {
+    const string = str.split("");
+    const vowel = ["a", "e", "i", "o", "u", "y"];
+    let firstVowel = "";
+    string.forEach((s, index) => {
+      if (firstVowel === "") {
+        if (vowel.includes(s)) {
+          firstVowel = index;
+        }
+      }
+    });
 
-      let firstVowel;
-      let flagNoConsoants = false;
-      if (regExp.test(stringInsert)) {
-        flagNoConsoants = true;
-      }
-      if (string[0] !== string[0].toLowerCase) {
-        setFlagUppercase(true);
-      }
-      string.forEach((s, index) => {
-          if (!firstVowel) {
-            if (vowel.includes(s)) {
-              firstVowel = index;
-            }
-          }
-      });
-      //flag it
-      let finalString;
-      if (firstVowel) {
-        const prefix = stringInsert.substring(0, firstVowel).toLowerCase();
-        const stem = stringInsert
-          .substring(firstVowel, stringInsert.length)
-          .toLowerCase();
-        // const stemArr = stem.split("");
-        // stemArr.reverse();
-        // const finalStem = stemArr.join("");
-        console.log(prefix);
-        console.log(stem);
-        // console.log(finalStem);
-        finalString = stem + prefix + "ay";
-      }
-      if (flagNoConsoants) {
-        finalString = stringInsert + "yay";
-      }
+    const prefix = str.substring(0, firstVowel).toLowerCase();
+    const stem = str.substring(firstVowel, stringInsert.length).toLowerCase();
+    console.log("requirement4: " + JSON.stringify({ prefix, stem }));
+    return requirement5({ prefix, stem });
+  };
+  const requirement5 = ({ prefix, stem } = {}) => {
+    const result = stem + prefix + "ay";
+    console.log("requirement5: " + result);
+    return result;
+  };
+  const requirement6 = (string) => {
+    const regexConsonants = /(?![aeiouy])[a-z]/gi;
+    let flagConsonant = false;
 
-      return finalString;
-      // console.log("final string "+ finalString);
-      // setStringArr(stringArr.concat(finalString));
-      // console.log(stringArr.concat(finalString));
+    string.forEach((s) => {
+      if (regexConsonants.test(s)) {
+        flagConsonant = true;
+      }
+    });
+    let response = string.join("");
+    if (!flagConsonant) {
+      console.log("requirement6: " + response + "yay");
+      return response + "yay";
+    } else {
+      console.log("requirement6: " + response);
+      return requirement4(response);
     }
   };
 
@@ -85,12 +113,7 @@ const Funcionality = () => {
             setStringInsert(e.target.value);
           }}
         />
-        <button onClick={(e) => program()}>Button</button>
-        <ul>
-          {stringArr.map((string) => {
-            <li>string</li>;
-          })}
-        </ul>
+        <button onClick={(e) => kisTest()}>Button</button>
       </header>
     </div>
   );
